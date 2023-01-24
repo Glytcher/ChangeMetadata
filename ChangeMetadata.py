886ff79c901b123b07c63d41b7146d3671dbb49d
Glytcher <28837783+Glytcher@users.noreply.github.com>
2023-01-24 23:45:19 +0100

    first commit

---
+++ ChangeMetadata.py
@@ -0,0 +1,137 @@
+import os
+from tqdm import tqdm
+import sys
+import time
+import toml
+
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from deezer import Client
+
+import mutagen
+from mutagen.flac import FLAC
+
+def main():
+    # Spotify API Credentials
+    with open('config.toml', 'r') as f:
+        config = toml.load(f)
+
+    client_id = config['SpotifyAPI']['client_id']
+    client_secret = config['SpotifyAPI']['client_secret']
+
+    # Check if client_id and client_secret are provided
+    if not client_id or not client_secret:
+        print("Error: client_id and/or client_secret not found in config file.")
+        input("Press any key to exit...")
+        sys.exit()
+
+    # Check if system arguments are provided
+    if len(sys.argv) < 2:
+        print("Error: Please provide a folder as a command line argument. You can do this by dragging and dropping a folder onto the python file.")
+        input("Press any key to exit...")
+        sys.exit()
+    else:
+        folder = sys.argv[1]
+
+    # load in all FLAC files from folder
+    flac_files = [os.path.join(folder, file) for file in os.listdir(folder) if file.endswith(".flac")]
+    if flac_files == []:
+        print("No FLAC files found in folder")
+        input("Press any key to exit...")
+        sys.exit()
+
+    # Setup Spotify API using Spotipy
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id, client_secret))
+    dz = Client()
+
+    # Ask for album or track link
+    link = input("Enter album or track link (Spotify or Deezer): ")
+    start_time = time.time()
+    if "spotify" in link:
+        # Retrieve Spotify metadata
+        try:
+            if 'album' in link:
+                album_metadata = sp.album(link)
+            if 'track' in link:
+                track_metadata = sp.track(link)
+        except spotipy.client.SpotifyException as e:
+            print(f"Error: {e}")
+            input("Press any key to exit...")
+            sys.exit()
+
+        if 'album' in link:
+            album_metadata = sp.album(link)
+            album_name = album_metadata['name']
+            tracks_metadata = album_metadata['tracks']['items']
+        else:
+            track_metadata = sp.track(link)
+            album_name = track_metadata['album']['name']
+            tracks_metadata = [track_metadata]
+        album_artists = [artist['name'] for artist in album_metadata['artists']]
+        release_date = album_metadata['release_date']
+
+        print(f"\033[1;32;40mThe following metadata will be applied:\033[0m")
+        print(f"\033[1;33;40mAlbum name:  \033[0m {album_name}")
+        print(f"\033[1;33;40mAlbum artist:\033[0m {', '.join(album_artists)}")
+        print(f"\033[1;33;40mRelease date:\033[0m {release_date}")
+    
+    elif "deezer" in link:
+        # Retrieve Deezer metadata
+        if "http" in link:
+            album_id = link.split("/")[-1]
+        else:
+            album_id = link
+
+        album = dz.get_album(album_id)
+        album_name = album.title
+        album_artists = ', '.join(str(x) for x in list([contributor.name for contributor in album.contributors]))
+        release_date = album.release_date
+        tracks_metadata = album.get_tracks()
+        print(f"\033[1;32;40mThe following metadata will be applied:\033[0m")
+        print(f"\033[1;33;40mAlbum name:  \033[0m {album_name}")
+        print(f"\033[1;33;40mAlbum artist:\033[0m {album_artists}")
+        print(f"\033[1;33;40mRelease date:\033[0m {release_date}")
+    else:
+        print("Invalid link. Please provide a valid Spotify or Deezer link.")
+        input("Press any key to exit...")
+        sys.exit()
+            # Check config file if user should be asked for confirmation
+    if config['Options']['alwaysAskForConformation']:
+        confirmation = input("Do you want to continue? (Y/n)\n") or "y"
+    else:
+        confirmation = "y"
+
+    if confirmation.lower() == "y":
+        if "spotify" in link:
+            for track_metadata, flac_file in tqdm(zip(tracks_metadata, flac_files), total=len(tracks_metadata), desc="Processing files"):
+                audio = mutagen.flac.FLAC(flac_file)
+                audio['album'] = album_name
+                audio["ARTIST"] = [artist['name'] for artist in track_metadata['artists']]
+                audio['title'] = track_metadata['name']
+                audio["ALBUMARTIST"] = album_artists
+                audio['date'] = release_date
+                audio.save()
+                
+        elif "deezer" in link:
+            for track, flac_file in tqdm(zip(tracks_metadata, flac_files), total=len(tracks_metadata), desc="Processing files"):
+                audio = mutagen.flac.FLAC(flac_file)
+                audio['album'] = album_name
+                audio["ARTIST"] = [contributor.name for contributor in track.contributors]
+                audio['title'] = track.title
+                audio["ALBUMARTIST"] = album_artists
+                audio['date'] = str(release_date)
+                audio.save()
+    end_time = time.time()
+    print(f"Metadata successfully applied!\nTotal time taken: {end_time - start_time:.2f} seconds")
+    input("Press Enter to exit...")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ README.md
@@ -0,0 +1,52 @@
+# Apply Spotify/Deezer metadata to FLAC files
+
+This script allows you to add Spotify/Deezer metadata to your FLAC files using Spotify/Deezer API. Useful for when you use both Foobar2000 and Spotify/Deezer and want to keep your metadata consistent for use with last.fm scrobbling. The script will add the following metadata to the FLAC files taken from Spotify/Deezer:
+
+- Album & track artist (Correctly handles multiple artists)
+- Album title
+- Track title
+- Release date
+
+## Requirements
+
+- Python 3.9+
+- [Spotipy](https://pypi.org/project/spotipy/)
+- [Deeezer-python](https://pypi.org/project/deezer-python/)
+- [Mutagen](https://pypi.org/project/mutagen/)
+- Spotify API credentials (client ID and client secret, see [here](https://developer.spotify.com/dashboard/applications) for more information)
+
+## Usage
+
+1. Clone the repository and navigate to the folder
+2. Install the dependencies by running `pip install -r requirements.txt`
+3. Create a `config.toml` file and add your Spotify API credentials to it. See the Configuration section below for more information.
+4. Run the script by providing the folder containing the FLAC files as a command line argument, e.g. `python ChangeMetadata.py "path/to/folder"`. This can also be done by dragging the folder onto the script file.
+5. Provide the Spotify link of the album when prompted
+6. The script will display the metadata that will be added and prompt for confirmation before applying
+
+## Configuration
+
+### config.toml template
+
+```toml
+[SpotifyAPI]
+client_id = ""      # Your client ID taken form the Spotify Developer Dashboard
+client_secret = ""  # Your client secret taken from the Spotify Developer Dashboard
+
+[Options]
+alwaysAskForConformation = true # If set to true, the program will always ask for confirmation before applying the metadata changes
+```
+
+You can configure the script by editing the `config.toml` file. The following options are available:
+
+- `alwaysAskForConfirmation`: If set to true, the script will always prompt for confirmation before applying the metadata. If set to false, the script will apply the metadata without prompting for confirmation.
+
+## Notes
+
+- Only tested on Windows with Python 3.9.5
+
+## Todo
+
+- [x] Add support for Deezer
+- [ ] Add disc metadata when available
+- [ ] Handle extended/alternative versions of albums not available on Spotify/Deezer
